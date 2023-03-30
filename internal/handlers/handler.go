2a7e31304272613f179b92cda3da27ab6deb56d5
Victor Chuguev <victorchuguev@gmail.com>
2023-03-30 13:35:29 +0400

    iter4 first try

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -14,12 +15,17 @@ import (
 
 type Repositories interface {
 	ReadURL(int) (string, error)
-	WriteURL(string) int
+	WriteURL(string) string
 }
 
 type Server struct {
 	urlMap  Repositories
 	handler http.Handler
+}
+
+type ShortJSON struct {
+	URL    string `json:"url,omitempty"`
+	Result string `json:"result,omitempty"`
 }
 
 func New(storage Repositories) *Server {
@@ -29,6 +35,7 @@ func New(storage Repositories) *Server {
 		handler: router,
 	}
 	router.HandleFunc("/", myServer.createShortURL).Methods(http.MethodPost)
+	router.HandleFunc("/api/shorten", myServer.shortenJSON).Methods(http.MethodPost)
 	router.HandleFunc("/{id:[0-9]+}", myServer.redirectToLongURL).Methods(http.MethodGet)
 
 	return myServer
@@ -39,14 +46,15 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) createShortURL(w http.ResponseWriter, r *http.Request) {
-	body, err := io.ReadAll(r.Body)
+	requestBody, err := io.ReadAll(r.Body)
 	if err != nil {
 		log.Println("error reading equest body:", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	defer r.Body.Close()
 
-	strBody := strings.TrimRight(string(body), "\n")
+	strBody := strings.TrimRight(string(requestBody), "\n")
 	if _, err = url.ParseRequestURI(strBody); err != nil {
 		err := fmt.Errorf("this \"%s\" is not URL", strBody)
 		log.Println(err)
@@ -54,10 +62,11 @@ func (s *Server) createShortURL(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	id := s.urlMap.WriteURL(strBody)
+	shortenedURL := s.urlMap.WriteURL(strBody)
 
+	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusCreated)
-	fmt.Fprint(w, "http://localhost:8080/", id)
+	fmt.Fprint(w, shortenedURL)
 }
 
 func (s *Server) redirectToLongURL(w http.ResponseWriter, r *http.Request) {
@@ -78,6 +87,47 @@ func (s *Server) redirectToLongURL(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "text/html")
 	w.Header().Set("Location", longURL)
 	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func (s *Server) shortenJSON(w http.ResponseWriter, r *http.Request) {
+	requestBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println("error reading equest body:", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	requestBodyJSON := ShortJSON{}
+
+	if err := json.Unmarshal(requestBody, &requestBodyJSON); err != nil {
+		log.Println("error unmarshaling JSON", err)
+		http.Error(w, "invalid data JSON", http.StatusBadRequest)
+		return
+	}
+
+	if requestBodyJSON.URL == "" {
+		log.Println("URL field is empty")
+		http.Error(w, "URL field is required", http.StatusBadRequest)
+		return
+	}
+
+	shortenedURL := s.urlMap.WriteURL(requestBodyJSON.URL)
+
+	requestBodyJSON = ShortJSON{
+		Result: shortenedURL,
+	}
+	responseBody, err := json.Marshal(requestBodyJSON)
+	if err != nil {
+		log.Println("error marshaling JSON", err)
+		http.Error(w, "invalid data JSON", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(responseBody)
 }
--- internal/handlers/handler_test.go
+++ internal/handlers/handler_test.go
@@ -16,76 +16,114 @@ func TestServer_createShortURL(t *testing.T) {
 	repo := storage.New()
 	srv := New(repo)
 	tests := []struct {
-		name        string
-		target      string
-		wantCode    int
-		requestBody string
-		wantBody    string
-		method      string
-		location    string
+		name            string
+		target          string
+		method          string
+		requestBody     string
+		wantBody        string
+		wantContentType string
+		wantCode        int
+		location        string
 	}{
 		{
-			name:        "case pos 1",
-			target:      "/",
-			wantCode:    http.StatusCreated,
-			requestBody: "https://www.google.com",
-			wantBody:    "http://localhost:8080/1",
-			method:      http.MethodPost,
-			location:    "",
+			name:            "case ok 1: post",
+			target:          "/",
+			method:          http.MethodPost,
+			requestBody:     "https://www.google.com",
+			wantBody:        "http://localhost:8080/1",
+			wantContentType: "text/html",
+			wantCode:        http.StatusCreated,
+			location:        "",
 		},
 		{
-			name:        "case pos 2",
-			target:      "/",
-			wantCode:    http.StatusCreated,
-			requestBody: "https://ya.ru",
-			wantBody:    "http://localhost:8080/2",
-			method:      http.MethodPost,
-			location:    "",
+			name:            "case ok 2: post",
+			target:          "/",
+			method:          http.MethodPost,
+			requestBody:     "https://ya.ru",
+			wantBody:        "http://localhost:8080/2",
+			wantContentType: "text/html",
+			wantCode:        http.StatusCreated,
+			location:        "",
 		},
 		{
-			name:        "case pos 3",
-			target:      "/1",
-			wantCode:    http.StatusTemporaryRedirect,
-			requestBody: "",
-			wantBody:    "",
-			method:      http.MethodGet,
-			location:    "https://www.google.com",
+			name:            "case ok 3: get",
+			target:          "/1",
+			method:          http.MethodGet,
+			requestBody:     "",
+			wantBody:        "",
+			wantContentType: "text/html",
+			wantCode:        http.StatusTemporaryRedirect,
+			location:        "https://www.google.com",
 		},
 		{
-			name:        "case neg 1",
-			target:      "/",
-			wantCode:    http.StatusBadRequest,
-			requestBody: "123",
-			wantBody:    "this \"123\" is not URL\n",
-			method:      http.MethodPost,
-			location:    "",
+			name:            "case ok 4: api/shortned",
+			target:          "/api/shorten",
+			method:          http.MethodPost,
+			requestBody:     "{\"url\":\"http://ya.ru\"}",
+			wantBody:        "{\"result\":\"http://localhost:8080/3\"}",
+			wantContentType: "application/json",
+			wantCode:        http.StatusCreated,
+			location:        "",
 		},
 		{
-			name:        "case neg 2",
-			target:      "/",
-			wantCode:    http.StatusMethodNotAllowed,
-			requestBody: "",
-			wantBody:    "",
-			method:      http.MethodPut,
-			location:    "",
+			name:            "case don't ok 1: wrong url",
+			target:          "/",
+			method:          http.MethodPost,
+			requestBody:     "123",
+			wantBody:        "this \"123\" is not URL\n",
+			wantContentType: "text/plain; charset=utf-8",
+			wantCode:        http.StatusBadRequest,
+			location:        "",
 		},
 		{
-			name:        "case neg 3",
-			target:      "/9223372036854775808",
-			wantCode:    http.StatusBadRequest,
-			requestBody: "",
-			wantBody:    "this ID: 9223372036854775808 is not valid\n",
-			method:      http.MethodGet,
-			location:    "",
+			name:            "case don't ok 2: wrong method",
+			target:          "/",
+			method:          http.MethodPut,
+			requestBody:     "",
+			wantBody:        "",
+			wantContentType: "",
+			wantCode:        http.StatusMethodNotAllowed,
+			location:        "",
 		},
 		{
-			name:        "case neg 4",
-			target:      "/10",
-			wantCode:    http.StatusNotFound,
-			requestBody: "",
-			wantBody:    "URL with ID 10 is not found in URLMap\n",
-			method:      http.MethodGet,
-			location:    "",
+			name:            "case don't ok 3: wrong id get",
+			target:          "/9223372036854775808",
+			method:          http.MethodGet,
+			requestBody:     "",
+			wantBody:        "this ID: 9223372036854775808 is not valid\n",
+			wantContentType: "text/plain; charset=utf-8",
+			wantCode:        http.StatusBadRequest,
+			location:        "",
+		},
+		{
+			name:            "case don't ok 4: wrong id",
+			target:          "/10",
+			method:          http.MethodGet,
+			requestBody:     "",
+			wantBody:        "URL with ID 10 is not found in URLMap\n",
+			wantContentType: "text/plain; charset=utf-8",
+			wantCode:        http.StatusNotFound,
+			location:        "",
+		},
+		{
+			name:            "case don't ok 5: invalid JSON",
+			target:          "/api/shorten",
+			method:          http.MethodPost,
+			requestBody:     "http://ya.ru",
+			wantBody:        "invalid data JSON\n",
+			wantContentType: "text/plain; charset=utf-8",
+			wantCode:        http.StatusBadRequest,
+			location:        "",
+		},
+		{
+			name:            "case don't ok 6: empty URL",
+			target:          "/api/shorten",
+			method:          http.MethodPost,
+			requestBody:     "{\"url\":\"\"}",
+			wantBody:        "URL field is required\n",
+			wantContentType: "text/plain; charset=utf-8",
+			wantCode:        http.StatusBadRequest,
+			location:        "",
 		},
 	}
 	for _, tt := range tests {
@@ -100,6 +138,7 @@ func TestServer_createShortURL(t *testing.T) {
 			defer response.Body.Close()
 			assert.Equal(t, tt.wantBody, string(userResult))
 			assert.Equal(t, tt.location, response.Header.Get("Location"))
+			assert.Equal(t, tt.wantContentType, response.Header.Get("Content-Type"))
 		})
 	}
 }
